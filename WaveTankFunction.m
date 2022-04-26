40d12bf3337d99c51fccc0142632d97ed63f71aa
daniel-h12345 <dhalpern4040@gmail.com>
2022-04-25 21:22:30 -0400

    added saving results as video
    a not elegant solution to saving the video, saves an image of a frame, rereads it and puts it in the video.

--- WaveTankFunction.m
+++ WaveTankFunction.m
@@ -1,10 +1,14 @@
 function [] = WaveTankFunction() 
 
+    outVideo = VideoWriter('outputVideo.mp4', 'MPEG-4');
+    outVideo.FrameRate = 10; 
+    open(outVideo);
 
-videoFReader = vision.VideoFileReader("wave_01.mov")
-%something's weird with .mp4
+videoFReader = vision.VideoFileReader("wave_03.mov")
 videoPlayer = vision.VideoPlayer;
+
 frameNum = 0
+
 while ~isDone(videoFReader)
   
   %Functions organized for basic bw image  
@@ -15,12 +19,6 @@ while ~isDone(videoFReader)
   %videoFrame = imgaussfilt(videoFrame, 3)
   %videoFrame = createMask(videoFrame)
   %videoFrame = edge(videoFrame, 'canny')
-
-  %% Notes
-  % Try making multiple masks
-  % Save to video
-  % nested color threshold
-  % show every stage to pinpoint problem
 
 
 
@@ -41,8 +39,6 @@ row = fillmissing(row, 'previous');
 if frameNum == 0
 calmLvl = lowY;
 end
-%disp(calmLvl)
-%
 
 
 
@@ -51,21 +47,32 @@ end
 test = edge(createMask(imgaussfilt(imcrop(videoFReader(), [100,600,1775,275]),5)), 'canny') + imcrop(videoFReader(), [100,600,1775,275]);
   %videoPlayer(insertShape(test, 'circle', [corX lowY 5], 'LineWidth', 10))
   withHighest = insertShape(test, 'circle', [corX lowY 5], 'LineWidth', 10);
-  videoPlayer(insertShape(withHighest,'line', [0 calmLvl 1776 calmLvl], 'LineWidth', 2, 'Color', 'red'));
+  video = insertShape(withHighest,'line', [0 calmLvl 1776 calmLvl], 'LineWidth', 2, 'Color', 'red');
+  %videoPlayer(video);
 
-  
+imwrite(video, frameNum + ".png") %peak efficency and is why there are so many files
+
+writeVideo(outVideo,imread(frameNum + ".png"));
 frameNum = frameNum+1
-%imwrite((edge(createMask(imgaussfilt(imcrop(videoFReader(), [100,600,1775,275]),3)), 'canny')) + imcrop(videoFReader(), [100,600,1775,275]), '%d.png', frameNum)
-
-
-
- % -=-=-= Main play video -=-=-=-=
- videoPlayer((edge(createMask(imgaussfilt(imcrop(videoFReader(), [100,600,1775,275]),3)), 'canny')) + imcrop(videoFReader(), [100,600,1775,275]))
-
- % Save image (needs work for video)
- %imwrite((edge(createMask(imgaussfilt(imcrop(videoFReader(), [100,600,1775,275]),3)), 'canny')), "test2.png")
-
-
 end
+
+
+
+
+%% notes
+%trace wave 
+% Search for x coordinate that is to the left of the wave
+% look how to use spline to make a smooth line and find local maxima and
+% minima
+% Make the curve smoother by averaging surrounding points'
+% rise over run to find slope between 2 pixels
+% when the slope is + and the other side is negative that is the maxima,
+% vice versa is minima
+%running average
+% signal processng toolbox
+% gaussian blur
+% convulution blur maybe if thats how it's spelled
+% find a 1d blur format
+
 end
 
