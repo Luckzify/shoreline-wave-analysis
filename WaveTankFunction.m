a2a9c274faefb075e18c9d46ca107fd5e11b950b
daniel-h12345 <dhalpern4040@gmail.com>
2022-08-02 20:18:04 -0400

    the startings of a curve fitting system
    not working yet but I wanted to upload as a backup incase anything goes wrong

--- WaveTankFunction.m
+++ WaveTankFunction.m
@@ -1,14 +1,13 @@
-function [] = WaveTankFunction() 
-
-    outVideo = VideoWriter('outputVideo.mp4', 'MPEG-4');
-    outVideo.FrameRate = 10; 
-    open(outVideo);
+%function [] = WaveTankFunction() 
+    %outVideo = VideoWriter('outputVideo.mp4', 'MPEG-4');
+    %outVideo.FrameRate = 10; 
+    %open(outVideo);
 
 videoFReader = vision.VideoFileReader("wave_03.mov")
 videoPlayer = vision.VideoPlayer;
 
 frameNum = 0
-
+x1 = 200
 while ~isDone(videoFReader)
   
   %Functions organized for basic bw image  
@@ -25,17 +24,42 @@ while ~isDone(videoFReader)
 %% Tracking highest point
 % closest attempt at tracking highest point, still needs to be more smooth
 white = edge(createMask(imgaussfilt(imcrop(videoFReader(), [100,600,1775,275]),3)), 'canny');
-row = nan(1,size(white,2)); 
-for k = 1:length(row) 
-    try
-        row(k) = find(white(:,k),1,'first');
-        %disp(row(k) + " " + k)
-        [lowY, corX] = min(row);
+
+row = nan(1,size(white,2));
+blank = nan(1,size(white,2));
+[a,b] = max(white);
+
+line = zeros(276, 1776);
+
+js = zeros(1, length(row));
+ls = zeros(1, length(row));
+for j = 1:length(row); 
+    for l = 1:276 %figure out how to get height
+        if white(l,j) == 1
+            line(l, j) = 1;
+            js(j) = j;
+            ls(j) = l;
+       
+            break
+        end
     end
 end
+F=sparse(line);
+createFit(js, ls)
+figure, spy(F)
+
 row = fillmissing(row, 'previous');
+
+
 %% Math section
-% Finding height of calm water
+
+% Finding height of calm water (figure out how to move to front of code to maybe make code faster)
+for k = 1:length(row); 
+    try 
+        row(k) = find(white(:,k),1,'first');
+        [lowY, corX] = min(row);
+    end    
+end
 if frameNum == 0
 calmLvl = lowY;
 end
@@ -43,36 +67,25 @@ end
 
 
 %% Video & yellow dot
-    %*Merge lines together when finished with this portion
-test = edge(createMask(imgaussfilt(imcrop(videoFReader(), [100,600,1775,275]),5)), 'canny') + imcrop(videoFReader(), [100,600,1775,275]);
-  %videoPlayer(insertShape(test, 'circle', [corX lowY 5], 'LineWidth', 10))
+  %*Merge lines together when finished with this portion
+  test = edge(createMask(imgaussfilt(imcrop(videoFReader(), [100,600,1775,275]),5)), 'canny') + imcrop(videoFReader(), [100,600,1775,275]);
   withHighest = insertShape(test, 'circle', [corX lowY 5], 'LineWidth', 10);
   video = insertShape(withHighest,'line', [0 calmLvl 1776 calmLvl], 'LineWidth', 2, 'Color', 'red');
-  %videoPlayer(video);
+  
+  videoPlayer(video);
+  
 
-imwrite(video, frameNum + ".png") %peak efficency and is why there are so many files
 
-writeVideo(outVideo,imread(frameNum + ".png"));
+ %imwrite(video, frameNum + ".png") %peak efficency and is why there are so many files
+
+ %writeVideo(outVideo,imread(frameNum + ".png"));
 frameNum = frameNum+1
-end
 
 
-
-
-%% notes
-%trace wave 
-% Search for x coordinate that is to the left of the wave
-% look how to use spline to make a smooth line and find local maxima and
-% minima
-% Make the curve smoother by averaging surrounding points'
-% rise over run to find slope between 2 pixels
-% when the slope is + and the other side is negative that is the maxima,
-% vice versa is minima
-%running average
-% signal processng toolbox
-% gaussian blur
-% convulution blur maybe if thats how it's spelled
-% find a 1d blur format
+%% IMPORTANT
+break
 
 end
+
+%end
 
