8ce20bdef9f9d9b25b4eb9a31aa346fab889905d
daniel-halpern <82973812+daniel-halpern@users.noreply.github.com>
2022-10-27 19:07:10 -0400

    Addition of curve fitting tool and more

--- WaveTankFunction.m
+++ WaveTankFunction.m
@@ -20,59 +20,81 @@ while ~isDone(videoFReader)
   %videoFrame = edge(videoFrame, 'canny')
 
 
+tempCrop = imcrop(videoFReader(), [500,600,1100,275]);
+imwrite(tempCrop, "Crop.png")
+
 
 %% Tracking highest point
 % closest attempt at tracking highest point, still needs to be more smooth
-white = edge(createMask(imgaussfilt(imcrop(videoFReader(), [100,600,1775,275]),3)), 'canny');
+%ORIGINAL CROP SIZE [100,600,1775,275]
+white = edge(createMask(imgaussfilt(imcrop(videoFReader(), [500,600,1100,275]),3)), 'canny');
 
 row = nan(1,size(white,2));
 blank = nan(1,size(white,2));
-[a,b] = max(white);
+[heightIMG,widthIMG] = size(white);
 
-line = zeros(276, 1776);
+line = zeros(heightIMG, widthIMG);
 
-js = zeros(1, length(row));
-ls = zeros(1, length(row));
-for j = 1:length(row); 
-    for l = 1:276 %figure out how to get height
+js = zeros(1, widthIMG-2);
+ls = zeros(1, widthIMG-2);
+for j = 1:widthIMG; 
+    for l = 1:heightIMG %figure out how to get height
         if white(l,j) == 1
             line(l, j) = 1;
-            js(j) = j;
-            ls(j) = l;
+            js(j-1) = j;
+            ls(j-1) = l;
        
             break
         end
     end
 end
+%pseduo code for sliding window
+% a = a + abs(pixel1 - pixel2)
+% AVGDividend = AVGDividend + 1
+AVGDividend = 0
+for l = 1:widthIMG-3
+    AVGDividend = AVGDividend + abs(ls(l) - ls(l+1));
+end
+AveragePixelChange = AVGDividend / (widthIMG - 3)
+%disp(widthIMG)
+%disp(ls)
 F=sparse(line);
 createFit(js, ls)
-figure, spy(F)
 
+%% Notes
+% Look into derivatives
+% Rule of powers for polynomials
+
+
+%% figure, spy(F)
+%disp("ls " + js)
 row = fillmissing(row, 'previous');
 
 
 %% Math section
 
 % Finding height of calm water (figure out how to move to front of code to maybe make code faster)
-for k = 1:length(row); 
+for k = 1:widthIMG; 
     try 
         row(k) = find(white(:,k),1,'first');
         [lowY, corX] = min(row);
     end    
 end
+
 if frameNum == 0
 calmLvl = lowY;
 end
 
 
 
+
 %% Video & yellow dot
   %*Merge lines together when finished with this portion
-  test = edge(createMask(imgaussfilt(imcrop(videoFReader(), [100,600,1775,275]),5)), 'canny') + imcrop(videoFReader(), [100,600,1775,275]);
+  test = edge(createMask(imgaussfilt(imcrop(videoFReader(), [500,600,1100,275]),5)), 'canny') + imcrop(videoFReader(), [500,600,1100,275]);
   withHighest = insertShape(test, 'circle', [corX lowY 5], 'LineWidth', 10);
-  video = insertShape(withHighest,'line', [0 calmLvl 1776 calmLvl], 'LineWidth', 2, 'Color', 'red');
+  video = insertShape(withHighest,'line', [0 calmLvl widthIMG+1 calmLvl], 'LineWidth', 2, 'Color', 'red');
   
-  videoPlayer(video);
+  %% videoPlayer(video);
   
 
 
@@ -83,7 +105,7 @@ frameNum = frameNum+1
 
 
 %% IMPORTANT
-break
+%break
 
 end
 
