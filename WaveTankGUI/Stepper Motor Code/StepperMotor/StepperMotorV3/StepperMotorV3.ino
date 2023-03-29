34f1781c1b21e95532fd332cd89cb40390c06363
irrelevantRyan <65325330+Rhyzhang@users.noreply.github.com>
2023-03-28 20:42:21 -0400

    working distance and speed increments

--- WaveTankGUI/Stepper Motor Code/StepperMotor/StepperMotorV3/StepperMotorV3.ino
+++ WaveTankGUI/Stepper Motor Code/StepperMotor/StepperMotorV3/StepperMotorV3.ino
@@ -15,27 +15,44 @@ void setup() {
 
   stepper.setCurrentPosition(0); 
 
-  speed = 0;
+  speed = 100;
   distance = 150;
 }
 
 void loop() {
-  stepper.setMaxSpeed(speed*10);
-  stepper.setAcceleration(speed*10);
-
   // Read Wave Type
   if(Serial.available()>0){
     value=Serial.read();
 
-    // Increment method where speed is plus 10 every signal received and tracking of speed is kept in Matlab
-    if(value == 2){
-      speed = speed + 10;
+    // Reset All Parameters
+    if(value == 0){
+      speed = 100;
+      distance = 150;
     } 
 
-    // Run Wave Type
-    /*Mono-Pulse Mode*/
-    // value of 1: pulse mode
+    // Speed Increments
     if(value == 1){
+      speed += 50;
+    } 
+    if(value == 2){
+      speed -= 50;
+    } 
+
+    // Distance Increments
+    if(value == 3){
+      distance += 10;
+    } 
+    if(value == 4){
+      distance -= 10;
+    } 
+
+
+    // Run Wave Type
+    /*MonoPulse Mode*/
+    // value of 255: pulse mode
+    if(value == 255){
+      stepper.setMaxSpeed(speed);
+      stepper.setAcceleration(speed);
 
       stepper.moveTo(distance);
       while (stepper.currentPosition() != distance) // Full speed up to 300
@@ -53,57 +70,51 @@ void loop() {
       // Now stopped after quickstop
     }
 
+    /*ConstantPulse Mode*/
+    // value of 254: Constant Pulse
+    if(value == 254){
+      stepper.setMaxSpeed(speed);
+      stepper.setAcceleration(speed);
+
+      stepper.moveTo(distance);
+      while (stepper.currentPosition() != distance) // Full speed up to 300
+        stepper.run();
+      stepper.stop(); // Stop as fast as possible: sets new target
+      stepper.runToPosition(); 
+      // Now stopped after quickstop
     
+      // Now go backwards
+      stepper.moveTo(0);
+      while (stepper.currentPosition() != 0) // Full speed basck to 0
+        stepper.run();
+      stepper.stop(); // Stop as fast as possible: sets new target
+      stepper.runToPosition(); 
+      // Now stopped after quickstop
+    }
 
+    // Run Wave Type
+    /*FastSlow Pulse Mode*/
+    // value of 253: FastSlow pulse mode
+    if(value == 253){
+      stepper.setMaxSpeed(speed);
+      stepper.setAcceleration(speed);
+
+      stepper.moveTo(distance);
+      while (stepper.currentPosition() != distance) // Full speed up to 300
+        stepper.run();
+      stepper.stop(); // Stop as fast as possible: sets new target
+      stepper.runToPosition(); 
+      // Now stopped after quickstop
+    
+      // Now go backwards
+      stepper.setMaxSpeed(100);
+      stepper.setAcceleration(100);
+      stepper.moveTo(0);
+      while (stepper.currentPosition() != 0) // Full speed basck to 0
+        stepper.run();
+      stepper.stop(); // Stop as fast as possible: sets new target
+      stepper.runToPosition(); 
+      // Now stopped after quickstop
+    }
   } 
-  // // Read Speed
-  // if(Serial.available()>0){
-  //   speed=Serial.read();
-  // } 
-  // // Read Distance
-  // if(Serial.available()>0){
-  //   distance=Serial.read();
-  // } 
-
-    /*Constant Pulse Mode*/
-    // 2000+ values are pulse mode
-    // Distance:2900+ ex: 2910 = 10 in distance
-    // Speed: 1000-1500
-    // if(value >= 2000){
-    // }
-
-
-  
-  // stepper.moveTo(150);
-  // while (stepper.currentPosition() != 150) // Full speed up to 300
-  //   stepper.run();
-  // stepper.stop(); // Stop as fast as possible: sets new target
-  // stepper.runToPosition(); 
-  // // Now stopped after quickstop
- 
-  // // Now go backwards
-  // stepper.moveTo(0);
-  // while (stepper.currentPosition() != 0) // Full speed basck to 0
-  //   stepper.run();
-  // stepper.stop(); // Stop as fast as possible: sets new target
-  // stepper.runToPosition(); 
-  // // Now stopped after quickstop
-
-
-
-  // stepper.moveTo(STEP_PER_REVOLUTION);
-  // while (stepper.currentPosition() != STEP_PER_REVOLUTION-50) // Full speed up to 300
-  //   stepper.run();
-  // stepper.stop(); // Stop as fast as possible: sets new target
-  // stepper.runToPosition(); 
-  // // Now stopped after quickstop
- 
-  // // Now go backwards
-  // stepper.moveTo(0);
-  // while (stepper.currentPosition() != 50) // Full speed basck to 0
-  //   stepper.run();
-  // stepper.stop(); // Stop as fast as possible: sets new target
-  // stepper.runToPosition(); 
-  // // Now stopped after quickstop
-
 }
