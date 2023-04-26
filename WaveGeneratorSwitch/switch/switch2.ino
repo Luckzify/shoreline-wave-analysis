9e98fdd33a6a40c70d03271d577fe59746bc10f1
badabingbadaboom <94488439+michaelsoft-wendows@users.noreply.github.com>
2023-04-26 19:23:59 -0400

    Update switch2.ino

--- WaveGeneratorSwitch/switch/switch2.ino
+++ WaveGeneratorSwitch/switch/switch2.ino
@@ -1,30 +1,30 @@
 #include <ezButton.h>
 #include <AccelStepper.h>
 
-ezButton button1(9);
-ezButton button2(11);
-ezButton button3(13);
-ezButton button4(3);
+ezButton button1(0);
+ezButton button2(1);
+ezButton button3(3);
 
 #define DEG_PER_STEP 1.8
 #define STEP_PER_REVOLUTION (360 / DEG_PER_STEP)
 AccelStepper stepper(AccelStepper::FULL4WIRE, 7, 6, 5, 4);
 const int steps_per_rev = 200;
 
-int oldb1,newb1,oldb2,newb2,oldb3,newb3,oldb4,newb4;
+bool oldb1,newb1,oldb2,newb2,oldb3,newb3;
 int mode;
 bool hold;
-bool b1toggled,b2toggled,b3toggled,b4toggled;
+bool b1toggled,b2toggled,b3toggled;
 
 void setup() {
   Serial.begin(9600);
   button1.setDebounceTime(50);
   button2.setDebounceTime(50);
   button3.setDebounceTime(50);
-  button4.setDebounceTime(50);
   pinMode(8,OUTPUT);
   pinMode(10,OUTPUT);
   pinMode(12,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(11,OUTPUT);
   pinMode(2,OUTPUT);
   stepper.setCurrentPosition(0);
 }
@@ -50,55 +50,57 @@ void varpulse(int speed1, int speed2, int distance, int iterations){
   }
 }
 
-void modes(){
-  if (mode == 1){
-    varpulse(1000,1000,150,1);
+void modes(int m){
+  if (m == 0){
+    varpulse(5000,5000,150,1);
   }
-  if (mode == 2){
-    varpulse(100,1000,150,1);
+  if (m == 1){
+    varpulse(5000,1000,150,1);
   }
-  if (mode == 3){
-    varpulse(1000,1000,150,5);
+  if (m == 2){
+    varpulse(2000,2000,150,5);
   }
 }
+
 void loop() {
   button1.loop();
   button2.loop();
   button3.loop();
-  button4.loop();
-  oldb1 = newb1, oldb2 = newb2, oldb3 = newb3, oldb4 = newb4;
-  newb1 = button1.isPressed(), newb2 = button2.isPressed(), newb3 = button3.isPressed(), newb4 = button4.isPressed();
-  b1toggled = (oldb1 == true && newb1 == false); 
+  oldb1 = newb1;
+  oldb2 = newb2;
+  oldb3 = newb3;
+  newb1 = button1.isPressed();
+  newb2 = button2.isPressed();
+  newb3 = button3.isPressed();
+  b1toggled = (oldb1 == true && newb1 == false);
   b2toggled = (oldb2 == true && newb2 == false);
   b3toggled = (oldb3 == true && newb3 == false);
-  b4toggled = (oldb4 == true && newb4 == false);
   digitalWrite(2,LOW);
-  if (b1toggled){
-    mode = 1;
+  int potentout = floor(analogRead(A1)*4/1023);
+  int mode = potentout;
+  if (mode == 0){
     digitalWrite(8,HIGH);
     digitalWrite(10,LOW);
     digitalWrite(12,LOW);
   }
-  if (b2toggled){
-    mode = 2;
+  if (mode == 1){
     digitalWrite(8,LOW);
     digitalWrite(10,HIGH);
     digitalWrite(12,LOW);
   }
-  if (b3toggled){
-    mode = 3;
+  if (mode == 2){
     digitalWrite(8,LOW);
     digitalWrite(10,LOW);
     digitalWrite(12,HIGH);
   }
-  if (b4toggled){
+  if (b3toggled){
     digitalWrite(2,HIGH);
-    modes();
+    modes(mode);
     while (true){
-      if (button4.getStateRaw() == 1){
+      if (button3.getStateRaw() == 1){
         break;
       }
-      modes();
+      modes(mode);
     }
     digitalWrite(2,LOW);
   }
